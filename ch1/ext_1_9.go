384499818a62bd093960e0eb44761ebc57a57b2d
Yu Hoshino <yhoshino11@gmail.com>
2022-05-10 21:18:03 +0900

    display status code, too.

---
+++ ch1/ext_1_9.go
@@ -0,0 +1,29 @@
+// display contents in URL
+// display HTTP status code, too.
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		response, err := http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+			os.Exit(1)
+		}
+		b, err := ioutil.ReadAll(response.Body)
+		response.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s", b)
+		statusCode := fmt.Sprintf("Status Code: %d", response.StatusCode)
+		fmt.Printf("%s", statusCode)
+	}
+}
