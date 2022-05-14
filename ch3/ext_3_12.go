0dfcd8e8a6c649f555a13b226d41192bd62ba224
Yu Hoshino <yhoshino11@gmail.com>
2022-05-14 15:24:31 +0900

    detect anagram

---
+++ ch3/ext_3_12.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"os"
+	"fmt"
+)
+
+func isAnagram(first string, second string) bool {
+	// 本来はbytes.Buffer等を使う。
+	aChars := make(map[string]int)
+	bChars := make(map[string]int)
+	for _, r := range first {
+		aChars[string(r)]++
+	}
+	for _, r := range second {
+		bChars[string(r)]++
+	}
+	for key, value := range aChars {
+		if bChars[key] != value {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	a := os.Args[1]
+	b := os.Args[2]
+	if len(a) != len(b) {
+		os.Exit(0)
+	}
+	if isAnagram(a, b) {
+		fmt.Println("anagram")
+	} else {
+		fmt.Println("not anagram")
+	}
+}
